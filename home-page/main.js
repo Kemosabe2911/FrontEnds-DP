09af72e857552df5dabbe0ff87fd76b67dd00ca9
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-15 19:59:33 +0530

    Add link to tiles

---
+++ home-page/main.js
@@ -0,0 +1,7 @@
+document.getElementById('buy').addEventListener('click',()  =>{
+    window.location.href="index.html";
+});
+
+document.getElementById('sell').addEventListener('click',()  =>{
+    window.location.href="index.html";
+});
