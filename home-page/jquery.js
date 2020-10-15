c21e4e6551292f3b6ac271cfee4fb69d8e26fe40
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-15 19:30:31 +0530

    Add comment to jquery

--- home-page/jquery.js
+++ home-page/jquery.js
@@ -1,9 +1,11 @@
+/*Toogle Menu*/
 $(function() {
     $(".toggle").on("click",function(){
         $(".items").toggleClass("show");
     });
 });
 
+/*Toggle Close*/
 $(document).ready(function(){
     $('.toggle').click(function(){
         $('.toggle').toggleClass('close');
