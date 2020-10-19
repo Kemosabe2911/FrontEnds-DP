e66ca4cb1a39bf593c0f83ce9a68f291ba503ade
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-19 16:44:32 +0530

    Display block property

--- sell-main.js
+++ sell-main.js
@@ -2,13 +2,16 @@
 document.getElementById('year1').addEventListener('click',()  =>{
     //window.location.href="#tiles.sell";
     var elmnt = document.getElementById("year1-section");
+    elmnt.style.display= 'block';
   elmnt.scrollIntoView();
   });
 
 //Year2
 document.getElementById('year2').addEventListener('click',()  =>{
     //window.location.href="#tiles.sell";
+    document.getElementById("year1-section").style.display='none';
     var elmnt = document.getElementById("year2-section");
+    elmnt.style.display= 'block';
   elmnt.scrollIntoView();
   });
 
