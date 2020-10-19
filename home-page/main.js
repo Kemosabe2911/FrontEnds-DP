bebab88a7312f345898843e2ef67c3c6f0c49c07
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-19 13:47:00 +0530

    Scroll effect danger

--- home-page/main.js
+++ home-page/main.js
@@ -1,7 +1,17 @@
 document.getElementById('buy').addEventListener('click',()  =>{
-    window.location.href="index.html";
+    //window.location.href="#tiles-sell";
+  var elmnt = document.getElementById("tiles-sell");
+  elmnt.scrollIntoView();
 });
 
 document.getElementById('sell').addEventListener('click',()  =>{
-    window.location.href="index.html";
+    //window.location.href="#tiles.sell";
+    var elmnt = document.getElementById("tiles-sell");
+  elmnt.scrollIntoView();
+});
+
+document.getElementById('home-btn').addEventListener('click',()  =>{
+  //window.location.href="#tiles.sell";
+  var elmnt = document.getElementById("nav_bar");
+elmnt.scrollIntoView();
 });
