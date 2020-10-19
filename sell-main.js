913c5186459fe14dab37428ca24843594a1c8332
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-19 16:33:34 +0530

    Add year2

--- sell-main.js
+++ sell-main.js
@@ -5,11 +5,24 @@ document.getElementById('year1').addEventListener('click',()  =>{
   elmnt.scrollIntoView();
   });
 
-//Home Button
+//Year2
+document.getElementById('year2').addEventListener('click',()  =>{
+    //window.location.href="#tiles.sell";
+    var elmnt = document.getElementById("year2-section");
+  elmnt.scrollIntoView();
+  });
+
+//Home Buttons
 
 document.getElementById('home-btn').addEventListener('click',()  =>{
     //window.location.href="#tiles.sell";
     var elmnt = document.getElementById("navbar");
   elmnt.scrollIntoView();
   });
-  
+ 
+document.getElementById('home-btn2').addEventListener('click',()  =>{
+    //window.location.href="#tiles.sell";
+    var elmnt = document.getElementById("navbar");
+  elmnt.scrollIntoView();
+  });
+   
