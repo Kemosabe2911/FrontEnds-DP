732e3da2e2197f9e7f5ed9e821956ae605addb59
Kemosabe2911 <stevinprince29@gmail.com>
2020-11-09 11:24:09 +0530

    Update nav

--- home-page/main.js
+++ home-page/main.js
@@ -1,3 +1,17 @@
+var myNav = document.getElementById('navbar');
+window.onscroll = function () { 
+    "use strict";
+    if (document.body.scrollTop >= 150 || document.documentElement.scrollTop >= 150 ){
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    } 
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+    }
+};
+
+
 document.getElementById('buy').addEventListener('click',()  =>{
     //window.location.href="#tiles-sell";
   var elmnt = document.getElementById("tiles-sell");
@@ -9,6 +23,7 @@ document.getElementById('sell').addEventListener('click',()  =>{
     var elmnt = document.getElementById("tiles-sell");
   elmnt.scrollIntoView();
 });
+
 
 //Home Button
 
